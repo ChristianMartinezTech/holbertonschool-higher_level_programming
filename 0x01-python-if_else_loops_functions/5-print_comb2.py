ce71940aca6383fd37ddc8bfdd4e4376bcd5acb1
ChristianMartinezTech <2388@holbertonschool.com>
2021-04-27 22:50:57 +0000

    Added the :02 range to the format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for x in range(0, 100):
+for x in range(00, 100):
     if x != 99:
-        print('{}, '.format(x), end='')
+        print('{:02d}, '.format(x), end='')
 
     else:
-        print('{}'.format(x), end='')
+        print('{}'.format(x))
