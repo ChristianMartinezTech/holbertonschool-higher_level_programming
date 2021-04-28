e9b51fb6b0860675d9e579e70df04740d3553926
ChristianMartinezTech <2388@holbertonschool.com>
2021-04-28 00:49:01 +0000

    Coded 7

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for x in range(9):
+    for y in range(9):
+
+        if (x != y) and (xy != 89):
+            print('{}{}, '.format(x)(y))
+
+        elif (x != y) and (xy == 89)
+            print('{}{}'.format(x)(y))
+            
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def islower(c):
+    x = ord(c)
+
+    if x >= 97 and x <= 122:
+        return True
+
+    else:
+        return False
