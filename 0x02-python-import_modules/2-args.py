b63778e8a1e7e1987af9345573ec21c76efe4c1f
ChristianMartinezTech <2388@holbertonschool.com>
2021-04-29 01:31:04 +0000

    Added the name line

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-import sys
-lng = (len(sys.argv) - 1)
-arg = sys.argv
+if __name__ == "__main__":
+    import sys
+    lng = (len(sys.argv) - 1)
+    arg = sys.argv
 
-if lng < 1:
-    print('0 arguments.')
+    if lng < 1:
+        print('0 arguments.')
 
-elif lng == 1:
-    print('1 argument:'.format(lng))
-    print('{}: {:s}'.format(lng, arg[1]))
+    elif lng == 1:
+        print('1 argument:'.format(lng))
+        print('{}: {:s}'.format(lng, arg[1]))
 
-else:
-    print('{:d} arguments:'.format(lng))
-    for x in range(1, len(sys.argv)):
-        print('{:d}: {:s}'.format(lng, arg[x]))
+    else:
+        print('{:d} arguments:'.format(lng))
+        for x in range(1, len(sys.argv)):
+            print('{:d}: {:s}'.format(lng, arg[x]))
