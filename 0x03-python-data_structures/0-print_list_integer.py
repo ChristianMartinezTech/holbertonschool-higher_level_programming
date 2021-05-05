1e9bec3842e3f6a08f14f6acc0127ff6e51a46f7
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-05 23:38:08 +0000

    Changes the print statement

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/python3
 def print_list_integer(my_list=[]):
-    for var in range(len(my_list)):
-        print('{}'.format(chr(var)))
+    for a in range(len(my_list)):
+        print("{}".format(my_list[a]))
