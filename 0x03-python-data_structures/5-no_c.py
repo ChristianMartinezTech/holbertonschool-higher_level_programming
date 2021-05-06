00fc1f1e8ed0bf300d5556115942ab81ecee4e02
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-06 03:29:47 +0000

    Coded 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+if __name__ == "__main__":
+    def no_c(my_string):
+        for a in range(len(my_string)):
+            if (my_string[a] == c) or (my_string[a] == C):
+                continue
+            else:
+                print('{}'.format(my_string[a]))
