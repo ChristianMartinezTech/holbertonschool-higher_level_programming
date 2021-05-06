54a0d99e72a840451dce8fae8cbcc586421514b6
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-06 03:32:30 +0000

    Changed the if statement, used commas in c and C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     def no_c(my_string):
         for a in range(len(my_string)):
-            if (my_string[a] == c) or (my_string[a] == C):
+            if (my_string[a] == 'c') or (my_string[a] == 'C'):
                 continue
             else:
                 print('{}'.format(my_string[a]))
