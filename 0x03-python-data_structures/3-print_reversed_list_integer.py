a1954543c1a3b275b915c8eabad5b087a701ec5e
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-06 02:45:26 +0000

    Reversed the string in the second line

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     for a in reversed(range(len(my_list))):
-        print('{}'.format(my_list[a]))
+        print('{:d}'.format(my_list[a]))
