a5459c13b12ad0afbcd326a86aecef86a12fc9ed
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-06 02:49:03 +0000

    Added the iff statement

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for a in reversed(range(len(my_list))):
-        print('{:d}'.format(my_list[a]))
+    if my_list:
+        for a in reversed(range(len(my_list))):
+            print('{:d}'.format(my_list[a]))
