e1a2f46caf140c709299531375bfea37f5d0c664
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-07 02:35:13 +0000

    Fixed the string copy

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix = list(matrix_2)
+    matrix_2 = list(matrix)
     for row in range(len(matrix_2)):
         for col in range(len(matrix_2)):
             return((matrix_2[row][col]) ** 2)
