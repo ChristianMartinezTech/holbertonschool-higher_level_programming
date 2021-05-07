05e2396d321deaf0131700a0d4ce2594864baa8b
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-07 02:30:40 +0000

    Fixed the list copy

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix = matrix_2
+    matrix = list(matrix_2)
     for row in range(len(matrix_2)):
         for col in range(len(matrix_2)):
             return((matrix_2[row][col]) ** 2)
