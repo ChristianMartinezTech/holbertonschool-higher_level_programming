cadbc1c5359d790f6482284993ef393779ed94ef
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-09 18:45:09 +0000

    Added the else statemement

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,4 +3,6 @@ def uniq_add(my_list=[]):
     for x in range(len(my_list)):
         if (my_list.count(x) >= 2):
             my_list.remove(x)
+        else:
+            continue
     return(sum(my_list))
