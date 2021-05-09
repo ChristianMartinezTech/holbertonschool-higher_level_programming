c78174a593bd1418cf30b4cc751b31f59490791c
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-09 17:50:07 +0000

    This is an exeperiement for the checker

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    [replace if x == search else x for x in new_list]
+    if x in new_list == search:
+        new_list[x] = replace
     return(new_list)
+    
+    """[replace if x == search else x for x in new_list]
+    return(new_list)"""
