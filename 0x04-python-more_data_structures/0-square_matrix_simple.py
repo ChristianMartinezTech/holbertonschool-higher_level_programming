ee992d2109ac1e8aaaf3c26fa1a7d5439dfe11cf
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-09 14:26:32 +0000

    Coded 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix_2 = matrix[:]
-    return((i ** 2) for i in matrix_2)
-    return(matrix)
+    matrix_2 = [x**2 for x in matrix[x]]
+    return(matrix_2)
+    
