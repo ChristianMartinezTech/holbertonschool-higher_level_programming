1b61a2a3e8aae632dc4701e9ddecd47c78c61c0e
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-09 17:57:47 +0000

    Coded  1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    if x in new_list == search:
-        new_list[x] = replace
-    return(new_list)
-    
-    """[replace if x == search else x for x in new_list]
-    return(new_list)"""
+    dic = {search: replace}
+
+    return([dic.get(n, n) for n in new_list])
