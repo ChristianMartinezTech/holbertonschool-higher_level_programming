8f2d5a258febca4a825631300127ce71c66835ee
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-10 16:43:14 +0000

    Added keya

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
-        return(a_dictionary.pop[key])
+        new_dict = a_dictionary.pop[key])
+        return(new_dict)
