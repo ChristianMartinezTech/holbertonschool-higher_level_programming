31fdab7e399a22c299d94a923dc23131a87cd760
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-10 16:44:24 +0000

    Changed the return

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for x in sorted(a_dictionary.keys()):
-        return('{}: {}'.format(x, a_dictionary[x]))
+        return('{}:'.format(a_dictionary[x]))
