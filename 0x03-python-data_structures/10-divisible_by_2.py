323b5ac2a672b739554684eb11a0582fad353275
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-10 19:03:06 +0000

    Coded 12

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     list_result = my_list.copy()
-    for x in my_list:
+    for x in range(len(my_list)):
         if (my_list[x] % 2) == 0:
             return(True)
         else:
