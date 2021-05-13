cdbf40b5d7b9d329891fafc1a9cef02ee61963d6
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-13 23:45:20 +0000

    Changing the for loop

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    final = " ".join(('{:d}'.format(a) for a in b) for b in matrix)
-    print(final)
+    for b in matrix:
+        final = " ".join('{:d}'.format(a) for a in b)
+        print(final)
