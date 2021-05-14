6b83c2cc88d91792329cfe46b0e40d2198aff647
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-14 02:41:46 +0000

    Addded the True and False to the return

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,6 +3,6 @@ def divisible_by_2(my_list=[]):
     list_result = my_list.copy()
     for x in range(len(my_list)):
         if (my_list[x] % 2) == 0:
-            return(True)
+            return(list_result, True)
         else:
-            return(False)
+            return(list_result, False)
