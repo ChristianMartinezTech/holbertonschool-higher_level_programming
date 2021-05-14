ebf3b01613e26ddb4ac1ca2267ef201128be0277
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-14 02:01:21 +0000

    Coded 8, waiting for checker

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    first = sentence(0)
+
+    if len(sentence) == 0:
+        first = None
+    return(length, first)
