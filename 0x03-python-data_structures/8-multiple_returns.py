899c905ad3caaf44b77282224eb0b7d538b08a41
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-14 02:05:15 +0000

    Added a second condition in the if

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@ def multiple_returns(sentence):
     length = len(sentence)
     first = sentence[0]
 
-    if len(sentence) == 0:
+    if len(sentence) == 0 or sentence[0] == '':
         first = None
     return(length, first)
