64f8a26112c8b624e1794d6653a149720b368655
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-14 00:47:39 +0000

    Removed the variable

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    res = tuple(map(lambda a, b: a + b, tuple_a, tuple_b))
-    return(res)
+    
+    return(tuple(map(lambda a, b: a + b, tuple_a, tuple_b)))
