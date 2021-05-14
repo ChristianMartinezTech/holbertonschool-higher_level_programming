5d1150f133c252e46861c7aa838b028739221bf2
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-14 03:20:34 +0000

    Coded 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    new_list = my_list.copy()
+    if (idx < 0) or (idx > (len(my_list) - 1)):
+        return(my_list)
+    else:
+        del new_list[idx]
+        return(new_list)
