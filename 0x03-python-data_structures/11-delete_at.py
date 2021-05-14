11782d7cc648afdfcaaf971a04c68d91e14ed113
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-14 03:24:12 +0000

    Added a condition to line 4

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     new_list = my_list.copy()
-    if (idx < 0) or (idx > (len(my_list) - 1)):
+    if (idx < 0) or (idx > (len(my_list) - 1)) or (not idx):
         return(my_list)
     else:
         del new_list[idx]
