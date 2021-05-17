5ea9e28d2bdb718f78a1a85545cfe21d9daa8cce
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-17 23:00:18 +0000

    Deleted the if value line

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if value:
-        try:
-            print("{:d}".format(int(value)))
-            return True
-        except (AttributeError, ValueError):
-            return False
+    try:
+        print("{:d}".format(int(value)))
+        return True
+    except (AttributeError, ValueError, TypeError):
+        return False
