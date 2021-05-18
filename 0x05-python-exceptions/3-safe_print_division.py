783aa11d1ad6e3535366f1027ac60123c5b3b350
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-18 02:46:29 +0000

    Coded 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    result = 0
     try:
         result = (a / b)
-    except ZeroDivisionError:
+    except (ZeroDivisionError):
         result = None
     finally:
         print("Inside result: {}".format(result))
