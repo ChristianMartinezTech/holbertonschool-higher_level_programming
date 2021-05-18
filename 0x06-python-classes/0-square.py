2f3669c193fe0be7ca7c79eb44746394321dea81
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-18 22:06:20 +0000

    Coded 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3 
 """function that creates an empty class Square that defines a square"""
 class Square: 
-    """class Square"""
+    """class created called Square"""
     def __init__(self): 
         """empty class Square that defines a square"""
         {} 
-        """empty item"""
+        """empty item inside of the square class"""
