d493fac29ee608eaba947a72d7a2470c7fa9fdd7
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-18 22:02:00 +0000

    Coded 0, notes in every line

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""function that creates an empty class Square that defines a square"""
 class Square:
+    """class Square"""
     def __init__(self):
         """empty class Square that defines a square"""
         {}
