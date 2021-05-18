34073eb016b5964a319f42739900230e74d12d65
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-18 22:50:46 +0000

    Coded 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class Square that defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """class Square that defines a square"""
+    def __init__(self, size):
+        """define class with the variable called size"""
+        self.__private = size
