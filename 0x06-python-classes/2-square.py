33b1eb82ba7d96f41b6f87c93a35a038d4bde894
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-19 02:16:43 +0000

    Coded 2, added the self.__ line

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,6 +6,7 @@ class Square:
     """class Square that defines a square"""
     def __init__(self, size=0):
         """definition made, will do the if cicles"""
+        self.__size = size
         if isinstance(size, int):
             if size < 0:
                 raise ValueError('size must be >= 0')
