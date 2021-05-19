423d96c35a56a48b2702976e09cb5510e733c741
ChristianMartinezTech <2388@holbertonschool.com>
2021-05-19 02:13:29 +0000

    Coded 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Functions that creates class Square that defines a square"""
+
+
+class Square:
+    """class Square that defines a square"""
+    def __init__(self, size=0):
+        """definition made, will do the if cicles"""
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError('size must be >= 0')
+        else:
+            raise TypeError('size must be an integer')
