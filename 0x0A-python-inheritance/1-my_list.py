027c1badfa824deec018f6b2967e960e4830b1e8
ChristianMartinezTech <crismartine.e@gmail.com>
2021-06-01 16:11:33 -0500

    Coded 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Excecutable"""
+
+
+class MyList(list):
+    """Class that will inherit from list"""
+    def print_sorted(self):
+        """Class that will inherit from list"""
+        print(sorted(self))
