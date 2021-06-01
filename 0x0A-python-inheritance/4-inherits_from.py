2db8449b71b417a10bf0bf430e5855c8d2e91da1
ChristianMartinezTech <crismartine.e@gmail.com>
2021-06-01 18:27:04 -0500

    Coded 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Python3 Execute statement"""
+
+
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of a class
+     that inherited (directly or indirectly) from the specified class"""
+    return issubclass(bool, a_class)
