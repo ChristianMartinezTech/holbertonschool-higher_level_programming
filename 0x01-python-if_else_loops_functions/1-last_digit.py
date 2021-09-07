5002a5e4f4225494653deb829ad79ed2417eea91
ChristianMartinezTech <crismartine.e@gmail>
2021-09-07 15:41:50 -0500

    Fixed the long line pep8 flag

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,10 +4,10 @@ number = random.randint(-10000, 10000)
 last = abs(number) % 10
 
 if (last) > 5:
-    print('Last digit of {} is {} and is greater than 5'.format(number, last))
+    print('Last digit of', number, 'is', last, 'and is greater than 5')
 elif (last) == 0:
-    print('Last digit of {} is {} and is 0'.format(number, last))
+    print('Last digit of', number, 'is', last, 'and is 0')
 elif number < 0:
-    print('Last digit of {} is -{} and is less than 6 and not 0'.format(number, last))
+    print('Last digit of', number, 'is', -last, 'and is less than 6 and not 0')
 else:
-    print('Last digit of {} is {} and is less than 6 and not 0'.format(number, last))
+    print('Last digit of', number, 'is', last, 'and is less than 6 and not 0')
