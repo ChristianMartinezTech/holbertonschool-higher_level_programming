12ffefbddf27011b36127d9c7bc07bc39eff9c7d
ChristianMartinezTech <crismartine.e@gmail>
2021-09-13 21:53:23 -0500

    Added double parenthesis for tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
 
     if not sentence:
         sentence[0] = None
     else:
-        return(tuple((length, first)))
+        return(tuple((length, sentence[0])))
