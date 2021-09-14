3440fe84652a68f3a9911c1e12007a58d42bbca4
ChristianMartinezTech <crismartine.e@gmail>
2021-09-14 18:39:19 -0500

    Coded 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     else:
         print('{:d} arguments:'.format(lng))
         for x in range(1, len(sys.argv)):
-            print('{:d}: {:s}'.format(lng, arg[x]))
+            print('{:d}: {:s}'.format(x, arg[x]))
