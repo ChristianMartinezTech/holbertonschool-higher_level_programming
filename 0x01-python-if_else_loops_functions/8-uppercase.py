cc8ff8d7f277f4201ebd5c9d43018e6cd03840d0
ChristianMartinezTech <crismartine.e@gmail>
2021-09-14 14:07:11 -0500

    Coded 102

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,8 @@ def uppercase(str):
     x = ord(str)
     mayus = (x - 32)
 
-    if x >= 97 and x <= 122:
-        print('{}'.format(chr(mayus), end='')
-
-    else:
-        continue
+    for a in len(str):
+        if (x >= 97) and (x <= 122):
+            print('{}'.format(chr(mayus), end='')
+        else:
+            continue
