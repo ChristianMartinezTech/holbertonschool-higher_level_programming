3dcbd6c1e5da41fa2ba81a0bc876fe1add76797d
ChristianMartinezTech <crismartine.e@gmail>
2021-09-14 13:34:18 -0500

    Changed the return statement

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
 
-    if length == 0:
-        return length, None
-    else:
+    if length != 0:
+        first = sentence[0]
         return (length, first)
+    else:
+        return length, None
