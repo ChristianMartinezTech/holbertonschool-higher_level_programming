d377ef6eb4ba531f2d6018fd82d10e3283207487
ChristianMartinezTech <crismartine.e@gmail>
2021-09-14 19:47:27 -0500

    Coded 100

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import hidden_4
-    for x in dir(hidden_4):
-        if x[0] == '_':
+    from hidden_4 import *
+
+    for x in dir():
+        if dir(x[0]) == '_':
             continue
         else:
-            print('{}'.format(line))
+            print(dir(x))
