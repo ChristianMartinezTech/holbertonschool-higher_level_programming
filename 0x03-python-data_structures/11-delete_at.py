d92761dabf01e4427200f1446e85c9727d0e8e0e
ChristianMartinezTech <crismartine.e@gmail>
2021-09-14 22:27:52 -0500

    Changed if statement

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     new_list = my_list.copy()
-    if (idx < 0) or (idx > (len(my_list) - 1)) or (not idx):
+    if (idx < 0) or (idx > (len(my_list) - 1)) or (idx is None):
         return(my_list)
     else:
         del new_list[idx]
