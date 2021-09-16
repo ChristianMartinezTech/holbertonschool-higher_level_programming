046e6a97f66859bb3489e929c267167d410900e1
ChristianMartinezTech <crismartine.e@gmail>
2021-09-15 21:48:08 -0500

    Changed the print statement

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    return(a_dictionary.update(key, value))
+    upd_dic = {key: value}
+
+    if (key is not None) or (value is not None):
+        return a_dictionary.update(upd_dic)
+    else:
+        return a_dictionary
