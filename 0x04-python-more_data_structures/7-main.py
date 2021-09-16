3e080ed210723ebecf7f3f3070231ee21fa79400
ChristianMartinezTech <crismartine.e@gmail>
2021-09-15 22:19:05 -0500

    Changed 6-print to compile 7-main

--- 0x04-python-more_data_structures/7-main.py
+++ 0x04-python-more_data_structures/7-main.py
@@ -4,7 +4,7 @@ print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_d
 
 a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
 new_dict = update_dictionary(a_dictionary, 'language', "Python")
-print_sorted_dictionary(new_dict)+
+print_sorted_dictionary(new_dict)
 print("--")
 print_sorted_dictionary(a_dictionary)
 
