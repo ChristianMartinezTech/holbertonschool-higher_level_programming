44c7a4c9b69cb35414511f982a7eb51ddb743e96
ChristianMartinezTech <crismartine.e@gmail>
2021-09-27 17:53:08 -0500

    Added some conditionals

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+
     try:
-        print("{:d}".format(int(value)))
-        return True
+        if value != "" and value != "-":
+            print("{:d}".format(int(value)))
+            return True
+        else:
+            return False
     except (AttributeError, ValueError, TypeError):
         return False
