6695c02cbad288090a99e4e5532c08f931a0ff0b
ChristianMartinezTech <crismartine.e@gmail>
2021-09-27 17:37:59 -0500

    Took some functions out of the for loops

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,9 +8,9 @@ def safe_print_list(my_list=[], x=0):
         for i in my_list:
             j += 1
 
-            if x >= j:
-                return my_list[-1]
-            else:
-                return(x)
+        if x >= j:
+            return my_list[-1]
+        else:
+            return(x)
     except (TypeError, IndexError):
         return(x)
