a3a0e4035088832467d27477dffa89e525b3ea30
ChristianMartinezTech <crismartine.e@gmail>
2021-09-27 17:24:35 -0500

    Coded 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     try:
         print(''.join(map(str, my_list[0:x])))
-        if x > len(my_list):
-            return my_list[-1]
-        else:
-            return(x)
+
+        for i in my_list:
+            if x > i:
+                return my_list[-1]
+            else:
+                return(x)
     except (TypeError, IndexError):
         return(x)
