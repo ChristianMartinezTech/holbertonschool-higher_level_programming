448c675f39ba093728dcf4d2266f1e16604f1795
ChristianMartinezTech <crismartine.e@gmail>
2021-09-27 17:29:28 -0500

    Coded a stronger list len

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,12 @@ def safe_print_list(my_list=[], x=0):
     try:
         print(''.join(map(str, my_list[0:x])))
 
+        j = 0
+
         for i in my_list:
-            if x > i:
+            j += 1
+
+            if x > j:
                 return my_list[-1]
             else:
                 return(x)
