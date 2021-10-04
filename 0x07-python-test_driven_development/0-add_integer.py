96c8f6719d772863cd77cba850424f326bc01817
ChristianMartinezTech <crismartine.e@gmail>
2021-10-04 10:58:50 -0500

    Coded 0, will create the test cases

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
     try:
-        int(a) #turning a and b in int
+        a = int(a) #turning a and b in int
     except:
         raise TypeError('a must be an integer')
 
     try:
-        int(b)
+        b = int(b)
     except:
         raise TypeError('b must be an integer')
 
-    return int(a) + int(b)
+    return a + b
