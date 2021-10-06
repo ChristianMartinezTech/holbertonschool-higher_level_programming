36df91b8262a56e6dd52404085c0525a9fa9679a
ChristianMartinezTech <crismartine.e@gmail>
2021-10-06 15:26:29 -0500

    Fixed pep8

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,42 +3,42 @@
 
 
 def matrix_divided(matrix, div):
-	"""function that divides all elements of a matrix"""
+    """function that divides all elements of a matrix"""
 
-	if type(matrix) != list:
-		raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if type(matrix) != list:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
-	for x in matrix:
-		if type(x) != list:
-			raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for x in matrix:
+        if type(x) != list:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
-	for x in matrix:
-		for y in range(len(x)):
-			if isinstance(y, (int, float)) == False:
-				raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for x in matrix:
+        for y in range(len(x)):
+            if not isinstance(y, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
-	if isinstance(div, (int, float)):
-		if div == 0:
-			raise ZeroDivisionError("division by zero")
-	else:
-		raise TypeError("div must be a number")
+    if isinstance(div, (int, float)):
+        if div == 0:
+            raise ZeroDivisionError("division by zero")
+    else:
+        raise TypeError("div must be a number")
 
-	if type(div) == bool:
-		raise TypeError("div must be a number")
+    if type(div) == bool:
+        raise TypeError("div must be a number")
 
-	rows_length = []
-	new_matrix = []
+    rows_length = []
+    new_matrix = []
 
-	for rows in matrix: #Checking the row length
-		rows_length.append(len(rows))
+    for rows in matrix:
+        rows_length.append(len(rows))
 
-	rows_length = set(rows_length)
+    rows_length = set(rows_length)
 
-	if len(rows_length) > 1:
-		raise TypeError("Each row of the matrix must have the same size")
+    if len(rows_length) > 1:
+        raise TypeError("Each row of the matrix must have the same size")
 
-	for i in matrix:
-		for j in i:
-			new_matrix.append(round(j/div, 2))
+    for i in matrix:
+        for j in i:
+            new_matrix.append(round(j/div, 2))
 
-	return new_matrix
+    return new_matrix
