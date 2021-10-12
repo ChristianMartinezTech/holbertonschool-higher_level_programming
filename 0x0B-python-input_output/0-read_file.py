250234f12412a3d2695f04073baebbc030622a4d
Christian Martinez <christianmartinez@pop-os.localdomain>
2021-10-12 16:57:11 -0500

    Corrected 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-""" Python interpreter location
-This makes the script run by default in python3"""
+""" Python interpreter location"""
 
 
 def read_file(filename=""):
-    """  function that reads a text file (UTF8) and prints it to stdout
-    """
-    with open(filename, encoding='utf-8') as store:
-        print(store.read())
+    """reads a text file (UTF8) and prints it to stdout"""
+
+    with open('my_file_0.txt', encoding='utf=8') as file:
+        print(file.read())
