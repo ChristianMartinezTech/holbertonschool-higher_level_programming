c1932b625cc55337e361b37d52f09890cbdcfd05
Christian Martinez <christianmartinez@pop-os.localdomain>
2021-10-12 17:44:23 -0500

    Coded 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout"""
 
-    with open(filename, encoding='utf-8') as file:
-        print(file.read())
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end="")
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """Python3 Location"""
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 import json
+
 
