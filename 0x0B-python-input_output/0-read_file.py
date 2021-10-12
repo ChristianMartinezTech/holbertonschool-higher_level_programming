534daa45da498d7e7547ce4fcbd1eef8b896f3e0
Christian Martinez <christianmartinez@pop-os.localdomain>
2021-10-12 17:06:37 -0500

    Coded 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout"""
 
-    with open('my_file_0.txt', encoding='utf=8') as file:
+    with open(filename, encoding='utf-8') as file:
         print(file.read())
