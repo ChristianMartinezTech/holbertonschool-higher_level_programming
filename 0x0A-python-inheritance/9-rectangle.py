884675147cec0d0c8a34d60f1ee0bf1ad6a9ea92
Christian Martinez <christianmartinez@pop-os.localdomain>
2021-10-13 15:57:16 -0500

    Fixed 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Python3 Interpreter Location"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class Rectangle that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+
+    def area(self):
+        return self.__width * self.__height
