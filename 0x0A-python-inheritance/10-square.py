675b9cab18060a7d5aee0b541ae08dbaff5faa20
Christian Martinez <christianmartinez@pop-os.localdomain>
2021-10-13 17:18:51 -0500

    Fixing 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Python3 Intepreter Location"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square that inherits from Rectangle"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
