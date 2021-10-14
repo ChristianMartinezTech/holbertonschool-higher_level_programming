5d55e6d1348662079b0f5b9e6963861833989602
Christian Martinez <christianmartinez@pop-os.localdomain>
2021-10-13 20:18:08 -0500

    Coded 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -7,12 +7,30 @@ class Square:
     def __init__(self, size=0):
         """Definition of size"""
         self.__size = size
-        if isinstance(size, int):
-            if size < 0:
-                raise ValueError('size must be >= 0')
-        else:
-            raise TypeError('size must be an integer')
 
     def area(self):
         """definition of area"""
-        return(self.__size * self.__size)
+        return self.__size ** 2
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print()
+        else:
+            i = 0
+            for i in range(self.__size):
+                for i in range(self.__size):
+                    print('#', end="")
+                print()
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(self.__size, int):
+            raise TypeError('size must be an integer')
+        if self.__size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
