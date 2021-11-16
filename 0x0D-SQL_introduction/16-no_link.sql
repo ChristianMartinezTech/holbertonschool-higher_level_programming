441f3fc582c06fd647324a5c2b1dcee8b2c52e3a
ChristianMartinezTech <crismartine.e@gmail.com>
2021-11-16 18:35:36 -0500

    Coded 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,5 @@
 -- Script that lists all records of the table second_table of the database hbtn_0c_0
-SELECT score, name IF EXISTS
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
