dd71f1f0f5f992d4c101d0db8ffd94ca67347b80
ChristianMartinezTech <crismartine.e@gmail.com>
2021-11-17 18:39:04 -0500

    Working on 2

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,5 @@
 -- Script that creates the table force_name
-CREATE table IF NOT EXIST force_name (id INT, name VARCHAR(256) NOT NULL);
+CREATE table IF NOT EXIST force_name (
+    id INT, 
+    name VARCHAR(256) NOT NULL
+);
