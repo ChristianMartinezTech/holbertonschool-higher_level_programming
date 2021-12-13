8a4000fcc88e31a0e396cacf5967adedd44ebc42
ChristianMartinezTech <crismartine.e@gmail.com>
2021-12-13 18:57:41 -0500

    Coded 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
 
+const argc = process.argv;
+
+if (argc.length <= 2) {
+  console.log('No argument');
+} else if (argc.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
