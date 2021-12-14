e57f63ae41f08d14fa0b4768ceb551d17f6c31eb
ChristianMartinezTech <crismartine.e@gmail.com>
2021-12-13 19:11:56 -0500

    Coded 3 and corrected 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const argc = process.argv;
+const arg = process.argv;
 
-if (argc.length <= 2) {
+if (arg.length <= 2) {
   console.log('No argument');
-} else if (argc.length === 3) {
+} else if (arg.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const arg = process.argv;
+
+if (arg[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(arg[2]);
+}
