2d1541c6bd43e3d7bd098b6aa6cb0beef9cabc19
ChristianMartinezTech <crismartine.e@gmail.com>
2021-12-14 23:57:14 -0500

    Coded 5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,9 +2,8 @@
 
 const Rectangle = require('./4-rectangle');
 
-class Square extends Rectangle { // Square inheriting from Rectangle
+module.exports = class Square extends Rectangle { // Square inheriting from Rectangle
   constructor (size) {
     super(size, size); // Bringing w and h as size
   }
-}
-module.exports = Square; // Exporting square to main
+};
