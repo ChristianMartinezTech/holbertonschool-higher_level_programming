d66d8facbd3bc858c57e6fbed806754460bf2233
ChristianMartinezTech <crismartine.e@gmail.com>
2021-12-14 21:39:20 -0500

    Coded 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+// Class Declaration
+module.exports = class Rectangle {
+  constructor (w, h) { // Constructor
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () { // print width and height with "X" character
+    for (let y = 0; y < this.height; y++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  double () { // print width and height with "X" TIMES TWO
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+    for (let y = 0; y < this.height; y++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () { // exchange width and height
+    for (let y = 0; y < this.width; y++) {
+      console.log('X'.repeat(this.height));
+    }
+  }
+};
