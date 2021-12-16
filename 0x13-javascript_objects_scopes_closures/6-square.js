632699246fbf718f6459bbaa161ed3bc3cefc7c0
ChristianMartinezTech <crismartine.e@gmail.com>
2021-12-15 20:03:12 -0500

    Coded 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const parentSquare = require('./5-square');
+
+module.exports = class Square extends parentSquare {
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.height));
+      }
+    } else {
+      this.print();
+    }
+  }
+};
