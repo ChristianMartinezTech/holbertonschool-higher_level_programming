739987dbdc242c081083fd62d8f98dc12a1261a9
ChristianMartinezTech <crismartine.e@gmail.com>
2021-12-15 20:44:57 -0500

    Coded 9

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+let count = 0;
+exports.logMe = function (item) {
+  if (item) {
+    const newArr = [];
+    newArr.push(item);
+
+    for (const i in newArr) {
+      console.log(count + ': ' + newArr[i]);
+      count += 1;
+    }
+  }
+};
