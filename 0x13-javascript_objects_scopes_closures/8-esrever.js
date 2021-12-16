23e035b6028d6a17100105494ace6c7e6c3c1836
ChristianMartinezTech <crismartine.e@gmail.com>
2021-12-15 20:29:14 -0500

    Coded 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+exports.esrever = function (list) {
+  const revArray = []; // New Reversed Array
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    revArray.push(list[i]);
+  }
+  return revArray;
+};
