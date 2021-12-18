5f0ea3b75223f9f45c1d21f47818bd1938c908c9
ChristianMartinezTech <crismartine.e@gmail.com>
2021-12-18 15:03:49 -0500

    Trying to fix holberton requeriments

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,21 +3,22 @@
 
 
 import MySQLdb
-import sys
+from sys import argv
 
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3],
-    charset="utf8"
-    )
+if __name__ == "__main__":
+    connetion = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8"
+        )
 
-cur = MySQLdb.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-db.close()
+    cur = MySQLdb.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    MySQLdb.close()
