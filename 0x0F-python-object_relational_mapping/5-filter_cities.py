7d09a300a93a161970f0d5428a90fe23579f17b4
ChristianMartinezTech <crismartine.e@gmail.com>
2021-12-19 15:05:51 -0500

    Coded 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     cur.execute("SELECT cities.name FROM cities\
     LEFT JOIN states ON cities.state_id = states.id\
     WHERE states.name = %s\
-    ORDER BY cities.id ASC;".format(argv[4]))
+    ORDER BY cities.id ASC;", (argv[4], ))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
