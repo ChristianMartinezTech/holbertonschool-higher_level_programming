83abaeccf45685e141db2b3d272a9ddab5b6138f
ChristianMartinezTech <crismartine.e@gmail.com>
2021-12-18 20:20:37 -0500

    Coded 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8"
+        )
+
+    cur = connection.cursor()
+    cur.execute("SELECT FROM cities\
+    WHERE cities.state_id LIKE BINARY '%{}%'\
+    ORDER BY id ASC;".format(argv[4]))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    connection.close()
