47383fe84d1fbe1b5cc7b6740d1e34660fb308c3
ChristianMartinezTech <crismartine.e@gmail.com>
2021-12-19 23:19:54 -0500

    Coded 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Script that lists all State objects from hbtn_0e_6_usa"""
+
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    q = session.query(State).order_by(State.id)
+
+    for states in q:
+        if State.id == '1':
+            print("{}: {}".format(states.id, states.name))
+
+    session.close()
