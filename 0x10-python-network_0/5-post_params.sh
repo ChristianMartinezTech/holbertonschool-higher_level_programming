670c3b2929ae6fecb120a62c2795bd356901b249
ChristianMartinezTech <crismartine.e@gmail.com>
2022-01-09 20:05:52 -0500

    Coded 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # POST request and displays the body of the response
-curl -s "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
