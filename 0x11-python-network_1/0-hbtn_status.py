6545a04d9e01c219265dfa9f75c131cded066169
ChristianMartinezTech <crismartine.e@gmail.com>
2022-01-11 17:33:34 -0500

    coded 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 """ script that fetches https://intranet.hbtn.io/status """
 
+from urllib import request
 
-
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: " + str(type(html)))
+        print("\t- content: " + str(html))
+        print("\t- utf8 content: " + str(html.decode('utf-8')))
