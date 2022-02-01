0da25f5ab9d02c44b00c8a6f0a9a5a961773feda
ChristianMartinezTech <crismartine.e@gmail.com>
2022-02-01 17:38:11 -0500

    Fixing style on 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,10 +6,8 @@ const fs = require('fs');
 const file = process.argv[2];
 const string = process.argv[3];
 
-fs.writeFile(file, string, 'utf-8', (err) => {
+fs.writeFile(file, string, 'utf-8', function (err) {
   if (err) {
     console.error(err);
-    return;
   }
-  return;
 });
