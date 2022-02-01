8f0a784acbdb79f2b5ce38022d76c416f84e767c
ChristianMartinezTech <crismartine.e@gmail.com>
2022-02-01 17:03:45 -0500

    Fixed style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
 // Including the fs module
-var fs = require('fs');
+const fs = require('fs')
 
-
-file = process.argv[2]
-fs.open(file, 'r', function (err, contents) {
-    if (err) {
-        return console.error(err);
-    }
-    console.log(contents);
-});
+fs.open(process.argv[2], 'r', function (err, contents) {
+  if (err) {
+    return console.error(err)
+  }
+  console.log(contents)
+})
